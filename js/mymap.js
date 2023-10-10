f7c6f558b0ca8e68e9e57024ed576edd40a74e73
HubashovD <34233868+HubashovD@users.noreply.github.com>
2023-10-10 22:04:54 +0300

    Update mymap.js

--- js/mymap.js
+++ js/mymap.js
@@ -1,6 +1,7 @@
-mapboxgl.accessToken = "pIRyhi5Ww2gupphkfRwc";
-// "vXHfK9IIJZ2VRIjeYQi6";
-// "pk.eyJ1IjoiZXZnZXNoYWRyb3pkb3ZhIiwiYSI6ImNsZGQyY2w5dDBjb2gzcG8xeXQ3c2EzczEifQ.lYkv8Hg7kFKNdNJF7wx4mg";
+mapboxgl.accessToken =
+  // "pIRyhi5Ww2gupphkfRwc";
+  // "vXHfK9IIJZ2VRIjeYQi6";
+  "pk.eyJ1IjoiZXZnZXNoYWRyb3pkb3ZhIiwiYSI6ImNsZGQyY2w5dDBjb2gzcG8xeXQ3c2EzczEifQ.lYkv8Hg7kFKNdNJF7wx4mg";
 
 const MAP_SETTINGS = {
   container: "map",
