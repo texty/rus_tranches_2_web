56c360b1e61492b0ac575068fef5d06561beee2a
HubashovD <34233868+HubashovD@users.noreply.github.com>
2023-10-11 12:18:10 +0300

    Update mymap.js

--- js/mymap.js
+++ js/mymap.js
@@ -205,6 +205,7 @@ function setupLayers() {
       "icon-size": ["interpolate", ["linear"], ["zoom"], 10, 0.05, 22, 0.05],
 
       "text-field": ["get", "name"],
+      "text-font": ["literal", ["Arial Unicode MS Regular"]],
       // "text-font": ["Open Sans Bold", "Arial Unicode MS Bold"],
       "text-size": ["interpolate", ["linear"], ["zoom"], 10, 12, 22, 12],
       "text-allow-overlap": true,
